58cde4c6ddbf6ef65503041e94cbda8bd52bb10e
David Sepúlveda V <david.sepulveda@hotmail.cl>
2021-03-29 01:47:51 -0300

    eslint suggestions fixed

--- utils/responses.js
+++ utils/responses.js
@@ -1,31 +1,32 @@
-const headersOutput = {
-    "Access-Control-Allow-Origin": "*",
-    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-    'Access-Control-Allow-Methods': 'GET,POST,PUT,OPTIONS',
-    "Access-Control-Allow-Credentials": true,
-  };
+'use strict'
 
-const generateResponse = (statusCode, body) => {
-    return {
-        isBase64Encoded: false,
-        statusCode: statusCode,
-        headers: headersOutput,
-        body: JSON.stringify(body)
-    };
+const headersOutput = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,OPTIONS',
+  'Access-Control-Allow-Credentials': true
 }
 
+const generateResponse = (statusCode, body) => {
+  return {
+    isBase64Encoded: false,
+    statusCode,
+    headers: headersOutput,
+    body: JSON.stringify(body)
+  }
+}
 
 const fixBody = (event) => {
-
-    if (event.hasOwnProperty('body') && typeof event.body === 'string') {
-        console.warn("Fixing body since it came as string");
-        event.body = JSON.parse(event.body);
-    }
-    event.fixed = true;
-    return event;
+  // eslint-disable-next-line no-prototype-builtins
+  if (event.hasOwnProperty('body') && typeof event.body === 'string') {
+    console.warn('Fixing body since it came as string')
+    event.body = JSON.parse(event.body)
+  }
+  event.fixed = true
+  return event
 }
 
 module.exports = {
-    generateResponse,
-    fixBody
+  generateResponse,
+  fixBody
 }
--- utils/utils.js
+++ utils/utils.js
@@ -1,45 +1,45 @@
-function getUniqueCharacters(episodeList) {
-  let characterList = [];
+function getUniqueCharacters (episodeList) {
+  const characterList = []
 
   episodeList.forEach((episode) => {
-    characterList.push(...episode.characters);
-  });
+    characterList.push(...episode.characters)
+  })
 
   return [
     ...new Set(
-      characterList.map((item) => parseInt(item.split("/").pop(), 10)).sort()
-    ),
-  ];
+      characterList.map((item) => parseInt(item.split('/').pop(), 10)).sort()
+    )
+  ]
 }
 
-function getLocationsInEpisode(episode, allCharacters) {
-	console.log('[getLocationsInEpisode] INI');
-	console.log('[getLocationsInEpisode] Params: ', {episode, allCharacters});
+function getLocationsInEpisode (episode, allCharacters) {
+  console.log('[getLocationsInEpisode] INI')
+  console.log('[getLocationsInEpisode] Params: ', { episode, allCharacters })
   const charactersInEpisode = episode.characters.map((item) =>
-    parseInt(item.split("/").pop(), 10)
-  );
-	console.log('[getLocationsInEpisode] Characters in episode', charactersInEpisode);
+    parseInt(item.split('/').pop(), 10)
+  )
+  console.log('[getLocationsInEpisode] Characters in episode', charactersInEpisode)
   const charactersData = allCharacters.filter((character) =>
     charactersInEpisode.includes(character.id)
-  );
-	console.log('[getLocationsInEpisode] Characters data', charactersData);
-  const locations = charactersData.map((item) => item.origin);
-	console.log('[getLocationsInEpisode] Locations', locations);
+  )
+  console.log('[getLocationsInEpisode] Characters data', charactersData)
+  const locations = charactersData.map((item) => item.origin)
+  console.log('[getLocationsInEpisode] Locations', locations)
 
-  let locationsSet = new Set();
-  let uniqueLocations = [];
+  const locationsSet = new Set()
+  const uniqueLocations = []
 
   locations.forEach((location) => {
     if (!locationsSet.has(location.name)) {
-      locationsSet.add(location.name, true);
-      uniqueLocations.push(location);
+      locationsSet.add(location.name, true)
+      uniqueLocations.push(location)
     }
-  });
+  })
 
-	console.log('[getLocationsInEpisode] Returning unique locations', uniqueLocations);
+  console.log('[getLocationsInEpisode] Returning unique locations', uniqueLocations)
 
-  return uniqueLocations;
+  return uniqueLocations
 }
 
-exports.getUniqueCharacters = getUniqueCharacters;
-exports.getLocationsInEpisode = getLocationsInEpisode;
+exports.getUniqueCharacters = getUniqueCharacters
+exports.getLocationsInEpisode = getLocationsInEpisode
